3d50f6deaf3997c1e2746934fa1163d3fae3293b
phanhisme <phanhhisme@gmail.com>
2023-10-20 11:53:04 +0700

    5. Updated SceneManager

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
-    //scenes reference 
+    //menu scene
     public GameObject titleScene;
     public GameObject charSelectionScene;
+    
+    //gameplay scene
     public GameObject battleScene;
     public GameObject chestScene;
     public GameObject restScene;
@@ -25,4 +27,49 @@ public class SceneManager : MonoBehaviour
     public GameObject playerIcon;
 
     GameManager _gameManager;
+    //BattleManager _battleManager;
+    //EndScreen endScreen;
+    //SceneFadeEffect sceneFadeEffect;
+
+    public enum Encounter { enemy, boss, restSite}
+
+    private void Awake()
+    {
+        _gameManager = GetComponent<GameManager>();
+        //_battleManager=FindObjectOfType<>
+        //endScreen=FindObjectOfType<>
+        //sceneFadeEffect=FindObjectOfType<>
+    }
+
+    //play button
+    public void PlayGame()
+    {
+        //hide title scene
+        titleScene.SetActive(false);
+    }
+
+    public void SelectCharacters(int i)
+    {
+        selectedChar = charList[i];
+        //changing the sprite only -> this will not change bg image
+        splashArt.sprite = selectedChar.featureImage;
+    }
+
+    public void StartGame()
+    {
+        //load map to choose path
+        StartCoroutine(LoadScene("Map"));
+        _gameManager.LoadCharacterStats();
+    }
+
+    public void SelectScene(string sceneName)
+    {
+        //StartCoroutine(LoadScene(sceneName));
+    }
+
+    public IEnumerator LoadScene(string sceneToLoad)
+    {
+        //Scene To Load here
+        yield return new WaitForSeconds(1f);
+    }
 }
